d4722722a26ac31ebe6b25d1bc51f5ee3db9cba8
unknown <rebecca.adefabi@pau.edu.ng>
2026-09-02 15:19:41 +0100

    my seco

---
+++ week-2/practice_2.rs
@@ -0,0 +1,10 @@
+fn main(){
+	println!(); // prints just a
+
+	println!("hello");
+
+	println!("format {} arguments", "some"); //prints format some arguments
+
+	println!("my name is {}. I am the class rep of Software Engineering 2026-2030", "Rebecca");
+	
+}
