e019c7152b6b4a84ae9258f5b9984d6842f9839c
unknown <rebecca.adefabi@pau.edu.ng>
2026-09-05 00:00:38 +0100

    calculation edit

--- week-2/project_2.rs
+++ week-2/project_2.rs
@@ -4,13 +4,17 @@ fn main(){
 	let p_2:f64 = 750_000.0; 
 	let p_3:f64 = 2_850_000.0; 
 	let p_4:f64 = 250_000.0;
+	let q_0:f64 = 2.0;
+	let q_1:f64 = 1.0;
+	let q_2:f64 = 3.0;
+	let q_3:f64 = 3.0;
+	let q_4:f64 = 1.0;
+
+	let _n:f64 = q_0 + q_1 + q_2 + q_3 + q_4;
 	let n:f64 = 5.0;
-	let s:f64 = p_0 + p_1 + p_2 + p_3 + p_4;
+	let s:f64 = (p_0*q_0) + (p_1*q_1) + (p_2*q_2) + (p_3*q_3) + (p_4*q_4);
 	let a:f64 = s/n;
 
 	println!("Sum = {}", s);
 	println!("Average = {}", a); 
-
-
-
 }
