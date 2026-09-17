4b20bdff8de0ac133529200ebc642ff73fe10bfa
unknown <rebecca.adefabi@pau.edu.ng>
2026-09-17 01:22:03 +0100

    remove target from practice_2

--- week-4/practice_2/target/debug/deps/practice_2.d
+++
@@ -1,5 +0,0 @@
-C:\Users\NEW\Desktop\Documents\r.adefabiCOS101\week-4\practice_2\target\debug\deps\practice_2.d: src\main.rs
-
-C:\Users\NEW\Desktop\Documents\r.adefabiCOS101\week-4\practice_2\target\debug\deps\practice_2.exe: src\main.rs
-
-src\main.rs:
